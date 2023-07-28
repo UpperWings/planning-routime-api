34125f1f2c98ad3ea6f15b47bf705eecea33c266
upperwings <rafaelborsatto58@gmai.com>
2023-07-28 19:21:44 -0300

    melhoria(Exception):incluida nova exceptio para tratar login com senha ou user invalidos

--- src/main/java/com/upperwings/planningroutime/api/configs/exception/HandlerException.java
+++ src/main/java/com/upperwings/planningroutime/api/configs/exception/HandlerException.java
@@ -1,13 +1,18 @@
-package com.upperwings.planningroutime.api.configs.security.exception;
+package com.upperwings.planningroutime.api.configs.exception;
 
-
-import org.hibernate.JDBCException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class HandlerException {
+
+    @ExceptionHandler(InternalAuthenticationServiceException.class)
+    public ResponseEntity handleInternalAuthenticationServiceExceptionException(Exception ex){
+        var err = "Usuario ou senha invalidos";
+        return ResponseEntity.badRequest().body(new ResponseExceptionDto(err, false));
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleDefaultException(Exception ex){
